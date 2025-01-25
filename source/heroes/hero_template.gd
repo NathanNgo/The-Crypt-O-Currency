9d0b98bc5fdf6a3cc8032905462a6785390a457d
Nathan Ngo <nathanj238@gmail.com>
2025-01-25 16:59:24 +1100

    Progress

---
+++ source/heroes/hero_template.gd
@@ -0,0 +1,82 @@
+extends Node2D
+
+enum EquipmentQuality {
+	WOOD,
+	IRON,
+	SILVER,
+	GOLD
+}
+
+enum EquipmentType {
+	SWORD,
+	SHIELD,
+	BOW,
+	QUIVER,
+	BOOK,
+	HAT,
+	STAFF,
+	SCROLL
+}
+
+enum HeroTypes {
+	FIGHTER,
+	ARCHER,
+	WIZARD,
+	MONK
+}
+
+@export var default_hero_sprite: Texture2D
+
+var hero_variants = [
+	{
+		"hero_type": HeroTypes.FIGHTER,
+		"hero_class_name": "Fighter",
+		"hero_health": 20,
+		"hero_attack": 2,
+		"equipment": [EquipmentType.SWORD, EquipmentType.SHIELD],
+		"equipment_quality": EquipmentQuality.IRON
+	},
+	{
+		"hero_type": HeroTypes.ARCHER,
+		"hero_class_name": "Archer",
+		"hero_health": 12,
+		"hero_attack": 4,
+		"equipment": [EquipmentType.BOW, EquipmentType.QUIVER],
+		"equipment_quality": EquipmentQuality.IRON
+	},
+	{
+		"hero_type": HeroTypes.WIZARD,
+		"hero_class_name": "Wizard",
+		"hero_health": 8,
+		"hero_attack": 6,
+		"equipment": [EquipmentType.BOOK, EquipmentType.HAT],
+		"equipment_quality": EquipmentQuality.IRON
+	},
+	{
+		"hero_type": HeroTypes.MONK,
+		"hero_class_name": "Monk",
+		"hero_health": 15,
+		"hero_attack": 3,
+		"equipment": [EquipmentType.STAFF, EquipmentType.SCROLL],
+		"equipment_quality": EquipmentQuality.IRON
+	},
+]
+
+
+var health: int
+var attack: int
+var hero_type: int
+
+var equipment: Array
+var equipment_quality: EquipmentQuality
+
+
+func _ready() -> void:
+	var hero_variant = hero_variants[randi_range(0, hero_variants.size() - 1)]
+
+	health = hero_variant["hero_health"]
+	attack = hero_variant["hero_attack"]
+	hero_type = hero_variant["hero_type"]
+	equipment = hero_variant["equipment"]
+	equipment_quality = hero_variant["equipment_quality"]
+	%HeroSprite.texture = default_hero_sprite
--- source/main.gd
+++ source/main.gd
@@ -1,11 +1,31 @@
 extends Node
 
+var coin_name: String
+var coin_bought: int
+var coin_value: int
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	%CoinCreation.submit_coin.connect(_on_submit_coin)
+	%SaleMenu.sale_complete.connect(_on_sale_complete)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	%SplashScreen.hide()
+	%SaleMenu.hide()
+	%Dungeon.hide()
+	%CoinCreation.hide()
+	%CoinCreation.show()
+
+
+func _on_submit_coin(coin_name_input: String) -> void:
+	coin_name = coin_name_input
+	%CoinCreation.hide()
+	%SaleMenu.show()
+	%SaleMenu.setup()
+	%SaleMenu.coin_name = coin_name
+
+
+func _on_sale_complete(coin_bought_input: int, coin_value_input: int) -> void:
+	coin_bought = coin_bought_input
+	coin_value = coin_value_input
+	%SaleMenu.hide()
+	%Dungeon.show()
